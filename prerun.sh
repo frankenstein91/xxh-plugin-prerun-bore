b4559376b144b4b010dc132370f4b2962237582d
Frank Tornack <frankenstein91.info@googlemail.com>
2022-05-28 19:23:16 +0200

    add export

--- prerun.sh
+++ prerun.sh
@@ -2,5 +2,5 @@ CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #############################
 # Example:
-#export PATH=$CURR_DIR:$PATH
+export PATH=$CURR_DIR:$PATH
 #############################
